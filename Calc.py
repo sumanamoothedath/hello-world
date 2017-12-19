59d4b665a4bd9f5f6675ef9252106ba82cf3e1de
sumanamoothedath <33067400+sumanamoothedath@users.noreply.github.com>
2017-12-19 20:22:10 +0530

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,32 +1,41 @@
+#!/usr/bin/env python
+
 def add(a, b):
-   return a + b
+    """Adds a and b and returns the sum."""
+    return(a + b)
 
 def subtract(a, b):
-   return a - b
+    """Subtracts a and b and returns the difference."""
+    return a - b
 
 def multiply(a, b):
-   return a * b
+    """Multiplies a and b and returns the product."""
+    return a * b
 
 def divide(a, b):
-   return a / b
+    """Divides a and b and returns the quotient."""
+    return a / b
 
-print("Select operation as 1 for Add, 2 for Subtract,3 for Multiply and 4 for Divide")
-
-choice = input("Enter choice(1,2,3 or 4):")
-
+if __name__ == "__main__":
+   print("Select operation as 1 for Add, 2 for Subtract,3 for Multiply and 4 for Divide")
+choice =input("Enter choice(1,2,3 or 4):")
 num1 = int(input("Enter num1: "))
 num2 = int(input("Enter num2: "))
 
 if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
+   print('{} + {} = '.format(num1, num2))
+   print(add(num1,num2))
 
 elif choice == '2':
-   print(num1,"-",num2,"=", subtract(num1,num2))
+    print('{} - {} = '.format(num1, num2))
+    print(subtract(num1,num2))
 
 elif choice == '3':
-   print(num1,"*",num2,"=", multiply(num1,num2))
+    print('{} * {} = '.format(num1, num2))
+    print(multiply(num1,num2))
 
 elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
+    print('{} / {} = '.format(num1, num2))
+    print(divide(num1,num2))
 else:
    print("Invalid input")
